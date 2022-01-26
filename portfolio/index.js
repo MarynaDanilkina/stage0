60894a71e2e5abc697655b639458b33ed9f84535
github-MarynaDanilkina <Danilkinamari99@gmail.com>
2022-01-26 09:48:32 +0300

    feat: js img

--- portfolio/index.js
+++ portfolio/index.js
@@ -21,3 +21,42 @@ function closeMenu() {
 
 
 console.log('1.48\n 2.15\n 3.22\n итого 85')
+
+
+
+/********* Замена изображения в portfolio ***********/
+
+
+const portfolioBtns = document.querySelector('.portfolio__header-buttons');
+portfolioBtns.addEventListener("click", changeImage);
+function changeImage(event) {
+	/*что целевой элемент event.target имеет класс portfolio-btn, то есть является кнопкой. */
+	if(event.target.classList.contains('_portfolio-btn')) {
+		/*Ближайший общий родитель кнопок с классом portfolio-btn - их общий контейнер portfolio__header-buttons. Находим его на странице и присваиваем переменной portfolioBtns */
+	  const portfolioBtn = document.querySelector('.portfolio__header-buttons'); 
+	  /*Найдём на странице все изображения с классом portfolio__header-photo и запишем результат поиска в переменную portfolioImages */
+	  const portfolioImages = document.querySelectorAll('.portfolio__header-photo img');
+	  const season = event.target.dataset.season 
+	  portfolioImages.forEach((img, index) => img.src = `../portfolio/assets/img/${season}/${index + 1}.jpg`);
+	}
+ }
+
+ /* Подсветка активной кнопки */
+
+ /*Найти все кнопки используя метод querySelectorAll() */
+
+ const portfolioButton = document.querySelectorAll('._portfolio-btn')
+const portfoliocontainer = document.querySelector('.portfolio__header-buttons')
+
+portfoliocontainer.addEventListener("click",  Buttonevent);
+
+ function Buttonevent(event) {
+	if(event.target.classList.contains('_portfolio-btn')) {
+		 portfolioButton.forEach(Butoo => {
+			 	Butoo.classList.remove('active');
+	})
+
+	const target = event.target;
+	target.classList.add('active') 
+}}
+
