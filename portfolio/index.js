59f8935f1ee9813644f885b84bcc3048cb694fb0
github-MarynaDanilkina <Danilkinamari99@gmail.com>
2022-01-28 14:59:42 +0300

    feat: js

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,12 +1,15 @@
 
 /*****Меню бургер*****/
-
+//бургер меню
 const iconMenu = document.querySelector('.header__menu-icon');
+//общий класс для навигации
 const menuHeader = document.querySelector('.header__menu');
 if(iconMenu){
-	
+	//при клике на бургер меню
 	iconMenu.addEventListener("click", function(e){
+		//блокируется весь экран
 		document.body.classList.toggle('_lock');
+
 		iconMenu.classList.toggle('_active');
 		menuHeader.classList.toggle('_active');
 	});
@@ -20,11 +23,59 @@ function closeMenu() {
 	
 }
 
+/*****Смена значка при разных темах светлая и темная*****/
+const moon = document.querySelector('.header__moon')
+const sun = document.querySelector('.header__sun')
+const icon = document.querySelector('.header__icon')
 
-console.log('1.48\n 2.15\n 3.22\n итого 85')
+	moon.addEventListener("click", function(a){
+		icon.classList.toggle('header__moon')
+		icon.classList.toggle('header__sun')
+		
+	})
+	const wrapper = document.querySelector('.wrapper')
+	const skills = document.querySelector('.skills__body-title')
+	const skillsTitle = document.querySelector('.skills__body-titles')
+	const portfolio = document.querySelector('.portfolio__body-title')
+	const portfolioTitle = document.querySelector('.portfolio__body-titles')
+	const video = document.querySelector('.video__body-title')
+	const videoTitle = document.querySelector('.video__body-titles')
+	const price = document.querySelector('.price__body-title')
+	const priceTitle = document.querySelector('.price__body-titles')
+	const footer = document.querySelector('.footer')
+	const footerA = document.querySelector('.footer__rss a')
+	const footerB = document.querySelector('.footer__github-github a')
+	const footerInsta = document.querySelector('.ico__insta')
+	const footerFb = document.querySelector('.ico__fb')
+	const footerTw = document.querySelector('.ico__tw')
+	const footerPinterest = document.querySelector('.ico__pinterest')
+	const headerTitle = document.querySelector('.header__hero-title')
+	const headerText = document.querySelector('.header__hero-text')
+	const languageList = document.querySelector('.language__list')
+	const portfolioBtn = document.querySelectorAll('._portfolio-btn')
+
+	
+
+const arr = [wrapper, skills, skillsTitle, portfolio, portfolioTitle, video, videoTitle, price, priceTitle, footer, footerA, footerB, footerInsta, footerFb, footerTw, footerPinterest];
+	
+const arr1 = [headerTitle, headerText, languageList];
 
 
+moon.addEventListener("click", function(c){
+	
+	
+	portfolioBtn.forEach(el=> But(el))
+	arr.forEach(el=> el.classList.toggle('__sun'))
+	
+	arr1.forEach(el=> el.classList.toggle('_sun'))
+	
 
+
+	
+})
+function But(el){
+	el.classList.toggle('sun')
+}
 /********* Замена изображения в portfolio ***********/
 
 
