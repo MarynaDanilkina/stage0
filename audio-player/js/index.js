e00182bc603aa811dfe7677a9c6c7aea5dd85219
github-MarynaDanilkina <Danilkinamari99@gmail.com>
2022-02-05 17:23:28 +0300

    feat: add prewSong, nextSong, audioPlay

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,30 +1,96 @@
+let allMusic = [
+	{
+		name: "Филипп Киркоров",
+		artict: "Стеснение Пропало",
+		img: "./assets/jpg/киркоров.jpg",
+		src: "./assets/audio/Киркоров.mp3"
+	},
+	{
+		name: "Миллион Алых Роз",
+		artict: "Алла Пугачева",
+		img: "./assets/jpg/Алла.jpg",
+		src: "./assets/audio/Пугачева.mp3"
+	},
+	{
+		name: "Танцуй Под Бузову",
+		artict: "Ольга Бузова",
+		img: "./assets/jpg/бузова.jpg",
+		src: "./assets/audio/Бузова.mp3"
+	},
+	
+];
 
-//музыка play pause
 
+
+const MusicImg = document.querySelector(".body__container img")
+const MusicName = document.querySelector(".song")
+const MusicArtist = document.querySelector(".tetle")
 const audio = document.querySelector('audio');
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+let musicIndex = 0;
+const play = document.querySelector('.play')
 
-const pause = document.querySelector('.pause');
+window.addEventListener("load", ()=>{
+	loadMusic(musicIndex)
+})
+
+
+function loadMusic(indexNumg){
+	MusicName.innerText = allMusic[indexNumg].name;
+	MusicArtist.innerText = allMusic[indexNumg].artict;
+	MusicImg.src = allMusic[indexNumg].img;
+	audio.src = allMusic[indexNumg].src;
+
+}
+
 let isPlay = false;
 function playAudio() {
-  isPlay = true;
- audio.currentTime = 0;
- audio.play();
-}
+	isPlay = true;
+	 audio.currentTime = 0;
+	 audio.play();
+	}
+	
+	function pauseAudio() {
+	isPlay = false;
+	 audio.pause();
+	}
+	
+	function audioPlay(){
+	  if(!isPlay){
+		  playAudio()
+		  document.getElementById("play").src="./assets/svg/pause.png";
+		  
+	  }else{
+		  pauseAudio()
+		  document.getElementById("play").src="./assets/svg/play.png";
+		  
+	  }
+	}
+	play.addEventListener('click', audioPlay);
 
-function pauseAudio() {
-isPlay = false;
- audio.pause();
-}
+	//следующая песня
 
-function audioPlay(){
-  if(!isPlay){
-	  playAudio()
-	  document.getElementById("play").src="./assets/svg/pause.png";
-	  
-  }else{
-	  pauseAudio()
-	  document.getElementById("play").src="./assets/svg/play.png";
-	  
-  }
+function nextSong(){
+	musicIndex++
+	if(musicIndex > allMusic.length -1){
+		musicIndex = 0
+	}
+	
+	loadMusic(musicIndex)
+	playAudio()
+	
 }
-play.addEventListener('click', audioPlay);
+next.addEventListener('click', nextSong )
+
+
+//песня назад
+function prevSong(){
+	musicIndex--
+	if(musicIndex < 0){
+		musicIndex = allMusic.length -1
+	}
+	loadMusic(musicIndex)
+	playAudio()
+}
+prev.addEventListener('click', prevSong )
