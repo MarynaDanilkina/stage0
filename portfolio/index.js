5da58ed07ddc717f95b5003bd85f42a787a55e1d
github-MarynaDanilkina <Danilkinamari99@gmail.com>
2022-02-07 18:19:07 +0300

    feat: the end

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,30 @@
 
+"use strict";
+/*сохраняем данные языка и темы в localStorage*/
+
+let themaFirst = "false";
+
+
+function setLocalStorage() {
+	
+  localStorage.setItem("thema", themaFirst);
+  
+}
+window.addEventListener("beforeunload", setLocalStorage)
+
+/* считываем данные языка и темы из localStorage*/
+function getLocalStorage() {
+  
+  if(localStorage.getItem("thema") === "true"){
+    changeSubject();
+  }
+ 
+}
+window.addEventListener('load', getLocalStorage)
+
+
+
+
 /*****Меню бургер*****/
 //бургер меню
 const iconMenu = document.querySelector('.header__menu-icon');
@@ -39,7 +65,13 @@ const moon = document.querySelector('.header__moon')
 const sun = document.querySelector('.header__sun')
 const icon = document.querySelector('.header__icon')
 
-	moon.addEventListener("click", function(a){
+moon.addEventListener("click", changeSubject);
+icon.addEventListener("click", changeSubject);
+
+
+
+function changeSubject() {
+		themaFirst = (icon.classList.contains("header__moon")).toString();
 		icon.classList.toggle('header__moon')
 		icon.classList.toggle('header__sun')
 	if(icon.classList.contains('header__sun')){
@@ -68,6 +100,8 @@ const icon = document.querySelector('.header__icon')
 		//portfolio
 		document.querySelectorAll('._portfolio-btn').forEach((el) => {
 			el.classList.add('sunWB') });
+		//video
+		document.querySelector(".screen").style.fill ="#000"
 		//price
 		document.querySelector('.price__header-body').style.color = "#000";
 		document.querySelectorAll('.price__header-button').forEach((el) => {
@@ -117,6 +151,8 @@ const icon = document.querySelector('.header__icon')
 		//portfolio
 		document.querySelectorAll('._portfolio-btn').forEach((el) => {
 			el.classList.remove('sunWB') });
+		//video
+		document.querySelector(".screen").style.fill ="#FFF"
 		//price
 		document.querySelector('.price__header-body').style.color = "#FFF";
 		document.querySelectorAll('.price__header-button').forEach((el) => {
@@ -138,7 +174,7 @@ const icon = document.querySelector('.header__icon')
 			el.style.fill = "#FFF"; });
 		
 	}	
-	})
+	}
 
 
 
@@ -227,3 +263,137 @@ function getTranslate(languages){
 
 })
 }}
+
+
+
+
+
+//video
+const video = document.querySelector('video')
+const videoPl = document.querySelector('.video-player')
+const proressArea = document.querySelector('.area')
+const playBt = document.querySelector('.playBt')
+const playVideo = document.querySelector('.video-play')
+const proress = document.querySelector('.input')
+
+
+
+let isPlay = false;
+function plVideo() {
+	document.getElementById("playBt").src="./assets/img/svg/pause.svg";
+	isPlay = true;
+	video.play();
+	playVideo.classList.add('none');
+	}
+	
+function pauseVideo() {
+	document.getElementById("playBt").src="./assets/img/svg/play.svg";
+	isPlay = false;
+	video.pause();
+	playVideo.classList.remove('none');
+}
+	
+function videoPlay(){
+	if(!isPlay){
+		plVideo()
+	}else{
+		pauseVideo()
+	}
+}
+
+
+	playBt.addEventListener('click', videoPlay);
+	videoPl.addEventListener('click', videoPlay);
+	playVideo.addEventListener('click', videoPlay);
+	
+
+
+
+
+	
+//прогресс цифры
+video.addEventListener("timeupdate", (e)=>{
+	
+	const currentTime = e.target.currentTime;
+	const duration = e.target.duration;
+	let progressWidth = (currentTime / duration) * 100;
+	proress.style.width = `${progressWidth}%`
+
+		let musicCurrentTime = document.querySelector(".current");
+		let musicDuration = document.querySelector(".length");
+
+		let audioDuration = video.duration;
+		let totalMin = Math.floor(audioDuration / 60);
+		
+		let totalSek = Math.floor(audioDuration %  60)
+		if(totalSek < 10){
+			totalSek = `0${totalSek}`
+		}
+		musicDuration.innerText = `${totalMin}:${totalSek}`;
+	
+		let currentMin = Math.floor(currentTime / 60);
+		let currentSek = Math.floor(currentTime %  60)
+		if(currentSek < 10){
+			currentSek = `0${currentSek}`
+		}
+		musicCurrentTime.innerText = `${currentMin}:${currentSek}`;
+	
+
+});
+// прогресс input
+
+
+proressArea.addEventListener("click", (e)=>{
+	let progressWidthval = proressArea.clientWidth;
+	let clickedOffSetX = e.offsetX;
+	let songDuration = video.duration;
+
+	video.currentTime = (clickedOffSetX / progressWidthval * songDuration)
+	plVideo() 
+})
+
+//на весь экран
+
+const controlFullScreen = document.querySelector('.player__fullscreen');
+controlFullScreen.addEventListener('click', goFullScreen);
+
+function goFullScreen(){
+	console.dir(video);
+	if(video.webkitSupportsFullscreen) video.webkitEnterFullScreen();
+ }
+ //звук
+ const controlVol = document.querySelector('.player__slider[name="volume"]');
+
+const volumButton = document.querySelector('.volum_active')
+
+
+
+ controlVol.addEventListener('change', updateVol);
+
+
+ volumButton.addEventListener("click",()=>{
+	volumButton.classList.toggle('activeVideo')
+	volumButton.classList.toggle('volumActive')
+	if(volumButton.classList.contains('activeVideo')){
+		video.volume = 0;
+		controlVol.style.display = 'none'
+	}
+	if(volumButton.classList.contains('volumActive')){
+		video.volume = 1;
+		controlVol.style.display = 'block'
+	}
+})
+
+
+
+ function updateVol(){
+ 	const volume = this.value;
+	video.volume = volume;
+}
+
+
+
+
+
+
+  
